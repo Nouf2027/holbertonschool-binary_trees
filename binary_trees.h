1a312e2691b85864de4d4d4eba1f325d94ba8ede
root <root@ip-10-42-27-184.ec2.internal>
2025-10-26 03:35:04 +0000

    Implement binary_tree_node function

---
+++ binary_trees.h
@@ -0,0 +1,18 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct binary_tree_s
+{
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
+} binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+#endif
